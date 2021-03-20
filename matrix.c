850f7385fcf1587dfceb543cbd72d127053ba34c
Brijeshlakkad <brijeshlakkad22@gmail.com>
2021-03-20 12:40:50 +0530

    multiplication with variable rows and columns of matrices

--- matrix.c
+++ matrix.c
@@ -4,28 +4,31 @@
 #include <mpi.h>
 #include <time.h>
 
-const int N = 3;
+const int ROWS = 32;
+const int COLUMNS = 3;
 
-void generateMatrix(int matrix[N][N]);
-void printMatrix(int matrix[N][N]);
-void multiply(int matrix1[N][N], int matrix2[N][N], int mul[N][N]);
+void generateMatrix(int rows, int columns, int matrix[rows][columns]);
+void printMatrix(int rows, int columns, int matrix[rows][columns]);
+void multiply(int rows1, int columns1, int matrix1[rows1][columns1],
+              int rows2, int columns2, int matrix2[rows2][columns2],
+              int mul[rows1][columns2]);
 
 int main(argc, argv) int argc;
 char *argv[];
 {
     int rank, size;
-    int matrix1[N][N];
-    int matrix2[N][N];
-    int mul[N][N];
-    generateMatrix(matrix1);
-    generateMatrix(matrix2);
+    int matrix1[ROWS][COLUMNS];
+    int matrix2[COLUMNS][ROWS];
+    int mul[ROWS][ROWS];
+    generateMatrix(ROWS, COLUMNS, matrix1);
+    generateMatrix(COLUMNS, ROWS, matrix2);
 
-    printMatrix(matrix1);
-    printMatrix(matrix2);
+    printMatrix(ROWS, COLUMNS, matrix1);
+    printMatrix(COLUMNS, ROWS, matrix2);
 
-    multiply(matrix1, matrix2, mul);
+    multiply(ROWS, COLUMNS, matrix1, COLUMNS, ROWS, matrix2, mul);
 
-    printMatrix(mul);
+    printMatrix(ROWS, ROWS, mul);
 
     MPI_Init(&argc, &argv);               /* starts MPI */
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); /* get current process id */
@@ -35,14 +38,16 @@ char *argv[];
     return 0;
 }
 
-void multiply(int matrix1[N][N], int matrix2[N][N], int mul[N][N])
+void multiply(int rows1, int columns1, int matrix1[rows1][columns1],
+              int rows2, int columns2, int matrix2[rows2][columns2],
+              int mul[rows1][columns2])
 {
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < rows1; i++)
     {
-        for (int j = 0; j < N; j++)
-        { 
+        for (int j = 0; j < columns2; j++)
+        {
             mul[i][j] = 0;
-            for (int k = 0; k < N; k++)
+            for (int k = 0; k < rows2; k++)
             {
                 mul[i][j] += matrix1[i][k] * matrix2[k][j];
             }
@@ -50,20 +55,20 @@ void multiply(int matrix1[N][N], int matrix2[N][N], int mul[N][N])
     }
 }
 
-void generateMatrix(int matrix[N][N])
+void generateMatrix(int rows, int columns, int matrix[rows][columns])
 {
     srand(time(NULL));
-    for (int row = 0; row < N; row++)
-        for (int column = 0; column < N; column++)
+    for (int row = 0; row < rows; row++)
+        for (int column = 0; column < columns; column++)
             matrix[row][column] = rand() % 100;
 }
 
-void printMatrix(int matrix[N][N])
+void printMatrix(int rows, int columns, int matrix[rows][columns])
 {
     printf("\n");
-    for (int row = 0; row < N; row++)
+    for (int row = 0; row < rows; row++)
     {
-        for (int column = 0; column < N; column++)
+        for (int column = 0; column < columns; column++)
         {
             printf("%d\t", matrix[row][column]);
         }
