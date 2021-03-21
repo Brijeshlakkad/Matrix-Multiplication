a77c7f074fe46c1b10bb9a24b4a21dba514c7176
Brijeshlakkad <brijeshlakkad22@gmail.com>
2021-03-21 14:44:23 +0530

    Timestamps and root_process and some conditions to prevent deadlocks

--- matrix.c
+++ matrix.c
@@ -6,8 +6,9 @@
 #include <limits.h>
 
 const int ROWS = 32;
-const int COLUMNS = 3;
+const int COLUMNS = 32;
 const int buffer_size = 2;
+const int root_process = 2;
 
 void generateMatrix(int rows, int columns, int matrix[rows][columns]);
 void printMatrix(int rows, int columns, int matrix[rows][columns]);
@@ -17,6 +18,7 @@ void multiplyMatrix(int rows1, int columns1, int matrix1[rows1][columns1],
 int multiply(int a, int b);
 void splitRow(int rows, int columns, int split_row, int matrix[rows][columns], int matrix_part[rows]);
 void inverseColumnToRow(int rows, int columns, int inv_column, int matrix[rows][columns], int inverse_row_matrix[rows]);
+void printDashedLine(int times);
 
 int main(argc, argv) int argc;
 char *argv[];
@@ -31,20 +33,22 @@ char *argv[];
     MPI_Comm_rank(MPI_COMM_WORLD, &process_rank); /* get current process id */
     MPI_Comm_size(MPI_COMM_WORLD, &process_size); /* get number of processes */
 
-    printf("Hello world from process %d of %d\n", process_rank, process_size);
-
-    if (process_rank == 0)
+    if (process_rank == root_process)
     {
+        float starting_time = MPI_Wtime();
+        printDashedLine(2);
+        printf("Starting time: %f", starting_time);
+        printDashedLine(2);
         int matrix1[ROWS][COLUMNS];
         int matrix2[COLUMNS][ROWS];
         int mul[ROWS][ROWS];
         generateMatrix(ROWS, COLUMNS, matrix1);
         generateMatrix(COLUMNS, ROWS, matrix2);
 
-        printMatrix(ROWS, COLUMNS, matrix1);
-        printMatrix(COLUMNS, ROWS, matrix2);
+        // printMatrix(ROWS, COLUMNS, matrix1);
+        // printMatrix(COLUMNS, ROWS, matrix2);
 
-        int current_task = 0;
+        int current_task = root_process;
         for (int i = 0; i < ROWS; i++)
         {
             int matrix1_part[COLUMNS];
@@ -54,7 +58,7 @@ char *argv[];
 
                 if (current_task == process_size - 1)
                 {
-                    current_task = 0;
+                    current_task = root_process;
                 }
                 current_task++;
                 // printf("Sending to %d\n", current_task);
@@ -78,33 +82,47 @@ char *argv[];
         }
 
         // TERMINATES the processes.
-        for (int task_index = 1; task_index < process_size; task_index++)
+        for (int task_index = 0; task_index < process_size; task_index++)
         {
+            if (task_index == root_process)
+            {
+                continue;
+            }
             int exit_token = INT_MIN;
             MPI_Send(&exit_token, 1, MPI_INT, task_index, 0, MPI_COMM_WORLD);
         }
 
-        printMatrix(ROWS, ROWS, mul);
+        // printMatrix(ROWS, ROWS, mul);
+
+        float ending_time = MPI_Wtime();
+        printDashedLine(2);
+        printf("Ending time: %f", ending_time);
+        printDashedLine(2);
+
+        float calc_time = ending_time - starting_time;
+        printDashedLine(2);
+        printf("Took %f", calc_time);
+        printDashedLine(2);
         // printf("\n\nExpected\n");
         // multiplyMatrix(ROWS, COLUMNS, matrix1, COLUMNS, ROWS, matrix2, mul);
         // printMatrix(ROWS, ROWS, mul);
     }
 
     MPI_Status status;
-    if (process_rank != 0)
+    if (process_rank != root_process)
     {
         while (1)
         {
             int total_rows;
-            MPI_Recv(&total_rows, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&total_rows, 1, MPI_INT, root_process, 0, MPI_COMM_WORLD, &status);
             if (total_rows == INT_MIN)
             {
                 break;
             }
             int matrix1_part[total_rows];
             int matrix2_part[total_rows];
-            MPI_Recv(&matrix1_part, total_rows, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&matrix2_part, total_rows, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&matrix1_part, total_rows, MPI_INT, root_process, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&matrix2_part, total_rows, MPI_INT, root_process, 0, MPI_COMM_WORLD, &status);
 
             int product_matrix = 0;
             for (int row_index = 0; row_index < total_rows; row_index++)
@@ -114,10 +132,9 @@ char *argv[];
 
             // printf("Process %d received token %d\n", process_rank, product_matrix);
 
-            MPI_Send(&product_matrix, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&product_matrix, 1, MPI_INT, root_process, 0, MPI_COMM_WORLD);
         }
     }
-    printf("Process %d EXITING\n", process_rank);
 
     MPI_Finalize();
     return 0;
@@ -179,5 +196,16 @@ void printMatrix(int rows, int columns, int matrix[rows][columns])
             printf("%d\t", matrix[row][column]);
         }
         printf("\n");
+    }
+}
+
+void printDashedLine(int times)
+{
+    int times_done = times;
+    printf("\n");
+    while (times_done > 0)
+    {
+        printf("-------------------------------\n");
+        times_done--;
     }
 }
