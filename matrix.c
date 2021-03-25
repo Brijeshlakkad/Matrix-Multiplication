3c4d10d4a88c9805b23fd407199e77e3f0cf9672
Brijeshlakkad <brijeshlakkad22@gmail.com>
2021-03-25 15:39:21 +0530

    matrix multiplication with row-major order using MPI_Scatter and MPI_Gather

--- matrix.c
+++ matrix.c
@@ -5,13 +5,12 @@
 #include <limits.h>
 // We will use the row-major order to store multidimensional arrays in linear storage such as random access memory.
 // This also helps to scatter the elements of the array and process them in more easy way.
-// Main benefit of this approach would be that time complexity will be O(log(N)) insteaad of O(N^2); where N is equal to the product of number of rows * columns.
 // Reference: https://en.wikipedia.org/wiki/Row-_and_column-major_order
 
 const int buffer_size = 2;
 const int root_process = 0;
 
-int *generateMatrix(int rows, int columns);
+void generateMatrix(int *matrix, int rows, int columns);
 void printMatrix(int *matrix, int rows, int columns);
 void multiplyMatrix(int rows1, int columns1, int matrix1[rows1][columns1],
                     int rows2, int columns2, int matrix2[rows2][columns2],
@@ -20,6 +19,8 @@ int multiply(int a, int b);
 int *inverseColumnToRow(int *matrix, int rows, int columns);
 // void inverseColumnToRow(int rows, int columns, int inv_column, int matrix[rows][columns], int inverse_row_matrix[rows]);
 void printDashedLine(int times);
+void print2DMatrix(int rows, int columns, int matrix[rows][columns]);
+void printPartialMatrix(int *matrix, int size);
 
 int main(argc, argv) int argc;
 char *argv[];
@@ -52,6 +53,20 @@ char *argv[];
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
+    float starting_time = MPI_Wtime();
+    int LENGTH_OF_METRIX = ROWS * COLUMNS;
+
+    int *matrix1;
+
+    // As second matrix going to be broadcasted, memory allocation should be done by every process.
+    int *matrix2;
+    if ((matrix2 = (int *)malloc(LENGTH_OF_METRIX * sizeof(int))) == NULL)
+    {
+        printf("Second matrix cannot be created!");
+        exit(1);
+    }
+    // int *inverse_matrix2;
+
     if (process_rank == root_process)
     {
         float starting_time = MPI_Wtime();
@@ -59,111 +74,90 @@ char *argv[];
         printf("Starting time: %f", starting_time);
         printDashedLine(2);
 
-        // Prepare matrices
-        int *resultant_matrix;
-        if ((resultant_matrix = malloc(ROWS * ROWS * sizeof(int))) == NULL)
+        if ((matrix1 = (int *)malloc(LENGTH_OF_METRIX * sizeof(int))) == NULL)
         {
-            printf("Resultant matrix cannot be created!");
+            printf("First matrix cannot be created!");
             exit(1);
         }
-        const int LENGTH_OF_METRIX = ROWS * COLUMNS;
-        int *matrix1 = generateMatrix(ROWS, COLUMNS);
-        int *matrix2 = generateMatrix(COLUMNS, ROWS);
+        generateMatrix(matrix1, ROWS, COLUMNS);
+        generateMatrix(matrix2, COLUMNS, ROWS);
 
-        // printMatrix(ROWS, COLUMNS, matrix1);
-        // printMatrix(COLUMNS, ROWS, matrix2);
+        printMatrix(matrix1, ROWS, COLUMNS);
+        printMatrix(matrix2, COLUMNS, ROWS);
 
-        int current_task = root_process;
-        int i = 0;
-        while (i < LENGTH_OF_METRIX)
-        {
-            i += ROWS;
-            if (current_task == process_size - 1)
-            {
-                current_task = root_process;
-            }
-            current_task++;
-            // printf("Sending to %d\n", current_task);
-
-            // Task id represents the process id.
-            int taskId = current_task % process_size;
-            int send_count = ROWS;
-            int *matrix1_part;
-            if ((matrix1_part = malloc(ROWS * sizeof(int))) == NULL)
-            {
-                printf("Resultant matrix cannot be created!");
-                exit(1);
-            }
-            matrix1_part = matrix1[i];
-
-            int matrix2_part = inverseColumnToRow(matrix2, COLUMNS, ROWS);
-
-            MPI_Send(&COLUMNS, 1, MPI_INT, taskId, 0, MPI_COMM_WORLD);
-
-            MPI_Send(matrix1_part, COLUMNS, MPI_INT, taskId, 0, MPI_COMM_WORLD);
-            MPI_Send(matrix2_part, COLUMNS, MPI_INT, taskId, 0, MPI_COMM_WORLD);
-            int recv_data;
-            MPI_Recv(&recv_data, 1, MPI_INT, taskId, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-            resultant_matrix[i - ROWS] = recv_data;
-        }
-
-        // TERMINATES the processes.
-        for (int task_index = 0; task_index < process_size; task_index++)
-        {
-            if (task_index == root_process)
-            {
-                continue;
-            }
-            int exit_token = INT_MIN;
-            MPI_Send(&exit_token, 1, MPI_INT, task_index, 0, MPI_COMM_WORLD);
-        }
-
-        // printMatrix(ROWS, ROWS, mul);
-
-        float ending_time = MPI_Wtime();
-        printDashedLine(2);
-        printf("Ending time: %f", ending_time);
-        printDashedLine(2);
-
-        float calc_time = ending_time - starting_time;
-        printDashedLine(2);
-        printf("Took %f", calc_time);
-        printDashedLine(2);
-        // printf("\n\nExpected\n");
-        // multiplyMatrix(ROWS, COLUMNS, matrix1, COLUMNS, ROWS, matrix2, mul);
-        // printMatrix(ROWS, ROWS, mul);
+        // *inverse_matrix2 = inverseColumnToRow(matrix2, COLUMNS, ROWS);
     }
 
-    MPI_Status status;
-    if (process_rank != root_process)
+    int send_count = LENGTH_OF_METRIX / process_size;
+
+    int matrix1_rows[send_count];
+    // if ((matrix1_rows = malloc(send_count * sizeof(int))) == NULL)
+    // {
+    //     printf("Receiving buffer for matrix 1 cannot be created!");
+    //     exit(1);
+    // }
+
+    MPI_Scatter(matrix1, send_count, MPI_INT, matrix1_rows, send_count, MPI_INT, root_process, MPI_COMM_WORLD);
+    MPI_Bcast(matrix2, LENGTH_OF_METRIX, MPI_INT, root_process, MPI_COMM_WORLD);
+
+    printf("matrix 1");
+    printPartialMatrix(matrix1_rows, send_count);
+    printf("matrix 2");
+    printPartialMatrix(matrix2, LENGTH_OF_METRIX);
+
+    // Columns of matrix 2
+    // Resultant product matrix will be the size of this columns of matrix 2.
+    int matrix2_columns = LENGTH_OF_METRIX / send_count;
+    int *product_matrix;
+    if ((product_matrix = malloc(matrix2_columns * sizeof(int))) == NULL)
     {
-        while (1)
+        printf("Resultant matrix cannot be created!");
+        exit(1);
+    }
+
+    for (int column_index = 0; column_index < matrix2_columns; column_index++)
+    {
+        product_matrix[column_index] = 0;
+        for (int row_index = 0; row_index < send_count; row_index++)
         {
-            int total_rows;
-            MPI_Recv(&total_rows, 1, MPI_INT, root_process, 0, MPI_COMM_WORLD, &status);
-            if (total_rows == INT_MIN)
-            {
-                break;
-            }
-            int matrix1_part[total_rows];
-            int matrix2_part[total_rows];
-            MPI_Recv(&matrix1_part, total_rows, MPI_INT, root_process, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&matrix2_part, total_rows, MPI_INT, root_process, 0, MPI_COMM_WORLD, &status);
-
-            int product_matrix = 0;
-            for (int row_index = 0; row_index < total_rows; row_index++)
-            {
-                product_matrix += matrix1_part[row_index] * matrix2_part[row_index];
-            }
-
-            // printf("Process %d received token %d\n", process_rank, product_matrix);
-
-            MPI_Send(&product_matrix, 1, MPI_INT, root_process, 0, MPI_COMM_WORLD);
+            // printf("(column_index) * (matrix2_columns) + row_index %d\n", (row_index) * (matrix2_columns) + column_index);
+            product_matrix[column_index] += matrix1_rows[row_index] * matrix2[(row_index) * (matrix2_columns) + column_index];
         }
     }
+    printf("product_matrix");
+    printPartialMatrix(product_matrix, matrix2_columns);
+
+    // Prepare matrices
+    int resultant_matrix[ROWS][ROWS];
+
+    // Gather the row sums from the buffer and put it in matrix C
+    MPI_Gather(product_matrix, matrix2_columns, MPI_INT, &resultant_matrix, ROWS, MPI_INT, root_process, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (root_process == process_rank)
+    {
+        printf("resultant_matrix");
+        print2DMatrix(ROWS, ROWS, resultant_matrix);
+    }
+
+    float ending_time = MPI_Wtime();
+    printDashedLine(2);
+    printf("Ending time: %f", ending_time);
+    printDashedLine(2);
+
+    float calc_time = ending_time - starting_time;
+    printDashedLine(2);
+    printf("Took %f", calc_time);
+    printDashedLine(2);
+    // printf("\n\nExpected\n");
+    // multiplyMatrix(ROWS, COLUMNS, matrix1, COLUMNS, ROWS, matrix2, mul);
+    // printMatrix(ROWS, ROWS, mul);
 
     MPI_Finalize();
+    if (root_process == process_rank)
+        free(matrix1);
+    free(matrix2);
     return 0;
 }
 
@@ -181,13 +175,16 @@ int *inverseColumnToRow(int *matrix, int rows, int columns)
         exit(1);
     }
     int new_index = 0;
-    for (int row_index = offset; row_index < columns; row_index = row_index + offset, new_index++)
+    for (int column_index = 0; column_index < columns; column_index++)
     {
-        matrix_part[new_index] = matrix[row_index];
+        for (int row_index = 0; row_index < rows; row_index++)
+        {
+            matrix_part[new_index] = matrix[(column_index + row_index) * columns];
+            new_index++;
+        }
     }
     return matrix_part;
 }
-
 
 // void inverseColumnToRow(int rows, int columns, int inv_row, int matrix[rows][columns], int inverse_row_matrix[rows])
 // {
@@ -214,22 +211,14 @@ void multiplyMatrix(int rows1, int columns1, int matrix1[rows1][columns1],
     }
 }
 
-int *generateMatrix(int rows, int columns)
+void generateMatrix(int *matrix, int rows, int columns)
 {
     srand(time(NULL));
-    int *matrix;
-    if ((matrix = malloc(rows * columns * sizeof(int))) == NULL)
-    {
-        printf("Matrix cannot be created!");
-        exit(1);
-    }
 
     for (int index = 0; index < rows * columns; index++)
     {
         matrix[index] = rand() % 100;
     }
-
-    return matrix;
 }
 
 void printMatrix(int *matrix, int rows, int columns)
@@ -237,12 +226,36 @@ void printMatrix(int *matrix, int rows, int columns)
     printf("\n");
     for (int row = 0; row < rows * columns; row++)
     {
+        if (row != 0 && (row % columns) == 0)
+        {
+            printf("\n");
+        }
+        printf("%d\t", matrix[row]);
+    }
+    printf("\n");
+}
+
+void print2DMatrix(int rows, int columns, int matrix[rows][columns])
+{
+    printf("\n");
+    for (int row = 0; row < rows; row++)
+    {
         for (int column = 0; column < columns; column++)
         {
-            printf("%d\t", matrix[row]);
+            printf("%d\t", matrix[row][column]);
         }
         printf("\n");
     }
+}
+
+void printPartialMatrix(int *matrix, int size)
+{
+    printf("\n");
+    for (int row = 0; row < size; row++)
+    {
+        printf("%d\t", matrix[row]);
+    }
+    printf("\n");
 }
 
 void printDashedLine(int times)
